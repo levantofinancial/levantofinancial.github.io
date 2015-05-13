018f275ed3645d37c2f72717162382aaeb6a27ea
Paige DeRaedt <paigederaedt@gmail.com>
2015-05-13 13:15:55 -0500

    attempting php

--- BeginHere.php
+++ BeginHere.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!DOCTYPE php>
 <html lang="en">
   <head>
     <meta charset="utf-8">
@@ -18,34 +18,7 @@
 <body data-spy="scroll" data-target="#myScrollspy">
 <div id="wrapper">
     <div id="header">
-      <nav class="navbar navbar-default navbar-fixed-top">
-        <div class="container-fluid">
-          <div class="navbar-header">
-          <!-- Brand and toggle get grouped for better mobile display -->
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#nav_collapse">
-              <span class="sr-only">Toggle navigation</span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-            </button>
-            <img class="brand" src="assets/levanto_logo3.png">
-          </div>
-
-          <!-- Collect the nav links, forms, and other content for toggling -->
-          <div class="collapse navbar-collapse" id="nav_collapse">
-            <ul class="nav navbar-nav">
-              <li><a style="color: white;" href="#"><b>HOME</b></a></li>
-              <li><a style="color: white;" href="#"><b>HOW IT WORKS</b></a></li>
-              <li><a style="color: white;" href="#"><b>TEAM</b></a></li>
-              <li><a style="color: white;" href="#"><b>MORE</b></a></li>
-            </ul>
-            <ul class="nav navbar-nav navbar-right">
-              <li><a style="color: white;" href="#"><b>SIGN UP</b></a></li>
-              <li><a style="color: white;" href="#"><b>SIGN IN</b></a></li>
-            </ul>
-          </div>
-        </div>
-      </nav>
+      <? include("navbar.php"); ?>
     </div>
 
 <div id="content_wrapper">
